bf3e9a95bd4f3900214b61167a7301e9755ca09e
dos <dos@6c6a7ac5-fc9d-4b1c-9e6c-becf412bdf1d>
2011-11-27 21:55:04 +0000

    tutek
    git-svn-id: https://dosowisko.net/svn/chrome@284 6c6a7ac5-fc9d-4b1c-9e6c-becf412bdf1d

--- My Time Organizer/js/tutorial.js
+++ My Time Organizer/js/tutorial.js
@@ -23,14 +23,59 @@ function second() {
 }
 
 function first() {
+	$('#tutorial').css("-webkit-transform","none").css('left', $('#tutorial').offset().left-200).css('top', $('#tutorial').offset().top-50);
+
+	wrap = document.createElement('div');
+	wrap.id = 'tutorialAddon';
+	$(wrap).css('float', 'left').css('width', '150px').css('height','225px').css('margin-right', '10px').css('background-color','white');
+	$(wrap).insertBefore($('#tutorialText'));
+	
+	next = document.createElement('div');
+	$(next).addClass('day_content').css('height','100%').css('width','100%').css('margin',0);
+	$(next).appendTo(wrap);
+
+	setDayHandlers(next);
+	
+	$("#tutorialText").html("My Time Organizer pozwala na zorganizowanie dnia przy pomocy trzech podstawowych typów elementów: wydarzeń, zadań oraz notatek. Dodawanie ich do wybranego dnia odbywa się na prostej zasadzie \"przeciągnij i upuść\".<br/><br/>Możesz wypróbować ten mechanizm na kolumnie obok. Najedź kursorem na wybrany przycisk, wciśnij lewy przycisk myszki, przesuń kursor na kolumnę po czym puść przycisk.");
 	elem = $('#add_panel');
-	$('#tutorialHighlight').animate({height: parseInt(elem.css('height'))+60, width: parseInt(elem.css('width'))+50, top: elem.offset().top-50, left: elem.offset().left-40}, 1000, second);
+	$('#tutorial').animate({top: elem.offset().top+70, left: elem.offset().left}, 1000);
+	
+	$('#tutorialHighlight').animate({height: parseInt(elem.css('height'))+60, width: parseInt(elem.css('width'))+50, top: elem.offset().top-50, left: elem.offset().left-40}, 1000);
+
+	
+	$('#tutorialNext')[0].onclick = null;
 }
 
 //$(document).ready(function() {
 function tutorialStart() {
+	$('#helper').hide();
 	//$('#tutorialHighlight').animate({height: 500, width: 30, top: 10, left: 10}, 5000, first);
+	okno = document.createElement('div');
+	okno.id = 'tutorial';
+	okno = $(okno);
+	okno.hide();
+	okno.addClass('helper');
+	okno.empty();
+	okno.css("z-index", 100000);
+	
+	text = document.createElement('div');
+	text.id = 'tutorialText';
+	text = $(text);
+	text.css('line-height', '1.2em');
+	text.html("Witaj w My Time Organizer! Ten samouczek pomoże Ci zapoznać się z funkcjami i zasadami działania aplikacji.");
+	text.appendTo(okno);
+	
+	next = document.createElement('div');
+	next.id = 'tutorialNext';
+	$(next).addClass('next_button');
+	next.innerHTML = 'Dalej';
+	next.onclick = first;
+	$(next).appendTo(okno);
+	
+	okno.appendTo("body");
+	okno.fadeIn(500);
+	
 	$('#tutorialHighlight').css('display','block').css('left', $('#tutorialHighlight').offset().left).css('top', $('#tutorialHighlight').offset().top);
-	first();
+	//first();
 }
 //);
