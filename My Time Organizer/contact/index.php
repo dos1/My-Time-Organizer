54efbd4faa5ee45fc858f4c99bd469bc32feb667
dos <dos@6c6a7ac5-fc9d-4b1c-9e6c-becf412bdf1d>
2011-11-28 16:16:02 +0000

    kontakt
    git-svn-id: https://dosowisko.net/svn/chrome@308 6c6a7ac5-fc9d-4b1c-9e6c-becf412bdf1d

---
+++ My Time Organizer/contact/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+/* [?] My Time Organizer @ Google Apps Hackathon 12.11.2011 (11/12/2011)
+ * [+] Authors: 
+ * - Dominik Galewski (mug3tsu)
+ * - Sebastian Krzyszkowiak (dos)
+ * - Krzysztof Marciniak (hun7er)
+ * [!] [alphabetical order]
+ */
+-->
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My Time Organizer</title>
+		<style>
+			html { background-color: whiteSmoke; }
+			body { text-align: center; font-family: Sans-Serif; margin: 0; padding-top: 5px; }
+			body::before {
+				content: "";
+				background-color: white;
+				display: block;
+				top: 0; left: 0;
+				width: 100%;
+				height: 110px;
+				position: absolute;
+				z-index: -1;
+			}
+		</style>
+</head>
+<body>
+	<p><img src="../images/logo.png" style="background-color: #5889d7" /></p>
+	<p style="margin-top: 40px">Wiadomość nie została wysłana.</p>
+	<p style="font-weight: bold">Dziękujemy!</p>
+</body>
+</html>
