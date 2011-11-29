162f0de5e02a7ca10a01e96cef48dbabae0b447d
dos <dos@6c6a7ac5-fc9d-4b1c-9e6c-becf412bdf1d>
2011-11-29 20:34:47 +0000

    ooo
    git-svn-id: https://dosowisko.net/svn/chrome@345 6c6a7ac5-fc9d-4b1c-9e6c-becf412bdf1d

--- My Time Organizer/js/script.js
+++ My Time Organizer/js/script.js
@@ -164,8 +164,7 @@ function nyanNyan() {
 		weeks['inner_table_right'].add("months", diff);
 		alignToMonday(true);
 		$('#inner_table_month').stop(false, true);
-		$('#inner_table_month').fadeOut(250, function() { $('#inner_table_month').empty(); fillMonthTable(document.getElementById('inner_table_month')); $('#inner_table_month').fadeIn(250); });
-		resizeDays();
+		$('#inner_table_month').fadeOut(250, function() { $('#inner_table_month').empty(); fillMonthTable(document.getElementById('inner_table_month')); $('#inner_table_month').fadeIn(250, resizeDays); });
 		updateWeek();
 	}
 
