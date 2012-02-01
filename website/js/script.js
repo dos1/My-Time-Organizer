8e147f5c7419d4eb88d1a77623d5ff9bf12d0618
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-02-01 22:03:22 +0100

    [website] (in Pinkie Pie singing voice) slider slider slider!

--- website/js/script.js
+++ website/js/script.js
@@ -1,11 +1,18 @@
 function resizeDays() {
  $(".day_content").css("height",$("#border_table").height()-10);
+ w = $("#slider").width();
+ h = w * (800/1280);
+ s = $("#slider ul li").length;
+ $("#slider, #slider ul li, #slider li img").css('width',w).css('height', h);
+ $("#slider ul li:first-child").css('display','none');
+ $("#slider ul").css('width', s*w);
+ s = $("#slider ul").attr('data-nr');
+ $("#slider ul").css('margin-left', "-"+s*w+"px");
 }
 
 window.onresize = resizeDays
 
 $(document).ready(function() {
-  resizeDays();
   $('.note').each(function() { $(this).css('backgroundColor', this.getAttribute('data-bgcolor')); });
   if (typeof chrome === 'undefined') {
     chrome = {}
@@ -19,6 +26,13 @@ $(document).ready(function() {
   }
         $('<div id="tutorialHighlight"><div class="hack"></div></div>').appendTo('body');
 
+$("#slider").easySlider({
+		auto: true,
+		continuous: true,
+		pause: 5000,
+		controlsShow: false
+	});
+resizeDays();
 });
 
         // font fix for Windows
