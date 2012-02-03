f82707e1e4abf780232be7ff1ca6247ec7065f08
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-02-03 22:40:05 +0100

    configuration reorganization

--- js/configuration.js
+++ js/configuration.js
@@ -2,33 +2,31 @@
 function setColor() {
 		localStorage['color']=$(this).attr('data-color');
 		loadUIColor();
-		end();
 }
 
-function confScreen() {
+function confColorThemeScreen() {
 
-	_gaq.push(['_trackPageview', '/config']);
-	
-	$('#helper').css("visibility", "hidden");
-	
-	okno = document.createElement('div');
+	_gaq.push(['_trackPageview', '/config/colorTheme']);
+
+	var okno = document.createElement('div');
 	okno.id = 'tutorial';
-	okno = $(okno);
+	okno = $('#tutorial');
 	okno.hide();
 	okno.addClass('helper');
 	okno.empty();
 	okno.css("z-index", 100000).css('width',400).css('padding-top',0);
 		
-	wrap = document.createElement('div');
+	var wrap = document.createElement('div');
 		
 	$(wrap).html(chrome.i18n.getMessage("colorTheme"));
 	$(wrap).find('p').css('text-align','center').css('padding-top',20);
 
-	next = document.createElement('div');
+	var next = document.createElement('div');
 	$(next).addClass('selectColorVersion');
 	$(next).css('background-color', '#457ad5');
 	next.setAttribute('data-color', 'blue');
 	next.onclick = setColor;
+	next.ondblclick = end;
 	$(next).appendTo($(wrap).find('p')[1]);
 
 	next = document.createElement('div');
@@ -36,6 +34,7 @@ function confScreen() {
 	$(next).css('background-color', '#fba8d3');
 	next.setAttribute('data-color', 'pink');
 	next.onclick = setColor;
+	next.ondblclick = end;
 	$(next).appendTo($(wrap).find('p')[1]);
 
 	next = document.createElement('div');
@@ -43,6 +42,7 @@ function confScreen() {
 	$(next).css('background-color', '#1a1a1a');
 	next.setAttribute('data-color', 'black');
 	next.onclick = setColor;
+	next.ondblclick = end;
 	$(next).appendTo($(wrap).find('p')[1]);
 	
 	next = document.createElement('div');
@@ -50,6 +50,7 @@ function confScreen() {
 	$(next).css('background-color', '#a88314');
 	next.setAttribute('data-color', 'brown');
 	next.onclick = setColor;
+	next.ondblclick = end;
 	$(next).appendTo($(wrap).find('p')[1]);
 
 	next = document.createElement('div');
@@ -57,6 +58,7 @@ function confScreen() {
 	$(next).css('background-color', '#a0e62f');
 	next.setAttribute('data-color', 'lime');
 	next.onclick = setColor;
+	next.ondblclick = end;
 	$(next).appendTo($(wrap).find('p')[1]);	
 	
 	next = document.createElement('div');
@@ -64,15 +66,89 @@ function confScreen() {
 	$(next).css('background-color', '#ff8b2a');
 	next.setAttribute('data-color', 'orange');
 	next.onclick = setColor;
+	next.ondblclick = end;
 	$(next).appendTo($(wrap).find('p')[1]);	
+
+	$(wrap).appendTo(okno);
+
+	next = document.createElement('div');
+	$(next).addClass('next_button');
+	next.innerHTML = chrome.i18n.getMessage("cancel");
+	next.onclick = confScreen;
+	$(next).css('width','100%').appendTo(okno);
+	
+	okno.appendTo("body");
+	okno.fadeIn(500);
+	
+	$('#tutorialHighlight').css('display','block').css('left', $('#tutorialHighlight').offset().left).css('top', $('#tutorialHighlight').offset().top);
+	//first();
+}
+
+function confImportExportScreen() {
+
+	_gaq.push(['_trackPageview', '/config/importExport']);
+
+	var okno = document.createElement('div');
+	okno.id = 'tutorial';
+	okno = $('#tutorial');
+	okno.hide();
+	okno.addClass('helper');
+	okno.empty();
+	okno.css("z-index", 100000).css('width',400).css('padding-top',0);
+		
+	var wrap = document.createElement('div');
+		
+	$(wrap).html("<p>Jeszcze tu nic nie ma.</p>");//chrome.i18n.getMessage("importExport"));
+	$(wrap).find('p').css('text-align','center').css('padding-top',20);
+
+	$(wrap).appendTo(okno);
+
+	next = document.createElement('div');
+	$(next).addClass('next_button');
+	next.innerHTML = chrome.i18n.getMessage("cancel");
+	next.onclick = confScreen;
+	$(next).css('width','100%').appendTo(okno);
+	
+	okno.appendTo("body");
+	okno.fadeIn(500);
+	
+	$('#tutorialHighlight').css('display','block').css('left', $('#tutorialHighlight').offset().left).css('top', $('#tutorialHighlight').offset().top);
+	//first();
+}
+
+function confScreen() {
+
+	_gaq.push(['_trackPageview', '/config']);
+	
+	$('#helper').css("visibility", "hidden");
+
+	$('#tutorial').remove();
+	
+	var okno = document.createElement('div');
+	okno.id = 'tutorial';
+	okno = $(okno);
+	okno.hide();
+	okno.addClass('helper');
+	okno.empty();
+	okno.css("z-index", 100000).css('width',250).css('padding-top',0);
+		
+	next = document.createElement('div');
+	$(next).addClass('next_button');
+	next.innerHTML = "Wersja kolorystyczna";
+	next.onclick = confColorThemeScreen;
+	$(next).css('width','100%').appendTo(okno);
+
+	next = document.createElement('div');
+	$(next).addClass('next_button');
+	next.innerHTML = "Import/eksport danych";
+	next.onclick = confImportExportScreen;
+	$(next).css('width','100%').appendTo(okno);
 	
 	next = document.createElement('div');
 	$(next).addClass('next_button');
 	next.innerHTML = chrome.i18n.getMessage("cancel");
 	next.onclick = end;
-	$(next).css('width','100%').appendTo(wrap);
-
-	$(wrap).appendTo(okno);
+	$(next).css('width','100%').appendTo(okno);
 	
 	okno.appendTo("body");
 	okno.fadeIn(500);
